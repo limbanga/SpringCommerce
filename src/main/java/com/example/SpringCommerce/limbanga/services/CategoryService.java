c05245389cb7f41d62d397c7b64762f52896b956
limbanga <95570833+limbanga@users.noreply.github.com>
2024-03-04 19:27:14 +0700

    tao project

--- src/main/java/com/example/SpringCommerce/limbanga/services/CategoryService.java
+++ src/main/java/com/example/SpringCommerce/limbanga/services/CategoryService.java
@@ -2,10 +2,12 @@ package com.example.SpringCommerce.limbanga.services;
 
 import com.example.SpringCommerce.limbanga.models.Category;
 import com.example.SpringCommerce.limbanga.repositories.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class CategoryService {
     CategoryRepository categoryRepository;
