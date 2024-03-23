49018f0334bf8eda462b542d274209f80ee8d034
limbanga <95570833+limbanga@users.noreply.github.com>
2024-03-23 17:40:09 +0700

    add filter in VariantController

--- src/main/java/com/example/SpringCommerce/limbanga/controllers/VariantController.java
+++ src/main/java/com/example/SpringCommerce/limbanga/controllers/VariantController.java
@@ -1,11 +1,13 @@
 package com.example.SpringCommerce.limbanga.controllers;
 
 import com.example.SpringCommerce.limbanga.models.ProductVariant;
-import com.example.SpringCommerce.limbanga.services.BaseService;
 import com.example.SpringCommerce.limbanga.services.ProductVariantService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/variants/")
@@ -17,4 +19,15 @@ public class VariantController
         this.productVariantService = productVariantService;
     }
 
+    @GetMapping("/filter-by")
+    public List<ProductVariant> filterBy(
+            @RequestParam(value = "productId", required = false) Long productId) {
+        // all record
+        var list = productVariantService.getAll();
+        // filter base condition
+        if (productId != null) {
+            list = list.stream().filter(x -> x.getProduct().getId().equals(productId)).toList();
+        }
+        return list;
+    }
 }
